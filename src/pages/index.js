ea1001d09716b37d0f9d61c0d0d26292f859674f
Gorkem Balcisoy <gbalcisoy@gmail.com>
2022-02-11 16:03:57 +0300

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>MERHABA SEVGİ HANIM</h1>
+    <h1>MERHABA </h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
